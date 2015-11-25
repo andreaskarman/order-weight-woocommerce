2fbed33ad0aecdde512bda218c11b3711f353592
Andreas Karman <andreas.karman@weupnorth.se>
2015-11-25 18:52:01 +0100

    Added check too see if WooCommerce is active. Fixes #5.

--- includes/class-woo-order-weight-activator.php
+++ includes/class-woo-order-weight-activator.php
@@ -4,7 +4,7 @@
  * Fired during plugin activation
  *
  * @link       http://wun.se
- * @since      1.0.0
+ * @since      0.1.0
  *
  * @package    Woo_Order_Weight
  * @subpackage Woo_Order_Weight/includes
@@ -15,7 +15,7 @@
  *
  * This class defines all code necessary to run during the plugin's activation.
  *
- * @since      1.0.0
+ * @since      0.1.0
  * @package    Woo_Order_Weight
  * @subpackage Woo_Order_Weight/includes
  * @author     WUN <andreas.karman@weupnorth.se>
@@ -27,10 +27,20 @@ class Woo_Order_Weight_Activator {
 	 *
 	 * Long Description.
 	 *
-	 * @since    1.0.0
+	 * @since    0.2.0
 	 */
 	public static function activate() {
-
+		// Check if WooCommerce is activated
+			if ( !in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+				
+				// Deactivate the plugin
+				deactivate_plugins(__FILE__);
+				
+				// Throw an error in the wordpress admin console
+				$error_message = __('This plugin requires <a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a>.', 'woocommerce');
+				die($error_message);
+				
+			}
 	}
 
 }
